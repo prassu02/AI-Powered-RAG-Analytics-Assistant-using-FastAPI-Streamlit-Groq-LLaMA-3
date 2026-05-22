1599aa8add7d0488ae8aca1c7ca883d949daaf12
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-22 15:27:59 +0530

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -2,37 +2,25 @@ from pypdf import PdfReader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
-# -----------------------------------
-# Load PDF File
-# -----------------------------------
-
 def load_file(file_path):
 
     text = ""
 
-    try:
+    if file_path.endswith(".pdf"):
+
         reader = PdfReader(file_path)
 
         for page in reader.pages:
-            page_text = page.extract_text()
 
-            if page_text:
-                text += page_text + "\n"
+            extracted = page.extract_text()
 
-    except Exception as e:
-        raise Exception(f"Error reading PDF: {str(e)}")
+            if extracted:
+                text += extracted
 
-    return text.strip()
+    return text
 
-
-# -----------------------------------
-# Split Text into Chunks
-# -----------------------------------
 
 def split_text(text):
-
-    if not text or len(text.strip()) == 0:
-        return []
 
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=500,
