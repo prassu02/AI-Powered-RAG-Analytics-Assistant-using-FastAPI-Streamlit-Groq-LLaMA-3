2152293a3d765a7c2429f92509006247cd452d64
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-05-22 14:28:32 +0530

    Update utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -1,36 +1,22 @@
 from pypdf import PdfReader
 
 # -----------------------------------
-# Load File
+# Load PDF File
 # -----------------------------------
 
 def load_file(file_path):
 
     text = ""
 
-    # PDF Files
     if file_path.endswith(".pdf"):
 
-        reader = PdfReader(file_path)
+        pdf = PdfReader(file_path)
 
-        for page in reader.pages:
-
+        for page in pdf.pages:
             page_text = page.extract_text()
 
             if page_text:
-                text += page_text + "\n"
-
-    # TXT Files
-    elif file_path.endswith(".txt"):
-
-        with open(file_path, "r", encoding="utf-8") as f:
-            text = f.read()
-
-    # CSV Files
-    elif file_path.endswith(".csv"):
-
-        with open(file_path, "r", encoding="utf-8") as f:
-            text = f.read()
+                text += page_text
 
     return text
 
@@ -39,6 +25,9 @@ def load_file(file_path):
 # -----------------------------------
 
 def split_text(text, chunk_size=500):
+
+    if not text:
+        return []
 
     chunks = []
 
